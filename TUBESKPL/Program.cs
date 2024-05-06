8192d8e9c34d3a0529b640f790e7d8ea333f8a85
alifzadanir <alifadanir@gmail.com>
2024-05-06 14:27:23 +0700

    Membuat menu

---
+++ TUBESKPL/Program.cs
@@ -0,0 +1,45 @@
+﻿//Generic
+
+using System.ComponentModel.Design;
+using TUBESKPL;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<DevicesAddConfig> devices = new List<DevicesAddConfig> ();
+        string pilihMenu;
+        string pilihRole;
+        Menu.Menus.menuUtama();
+
+        Console.WriteLine("Pilih role sesuai dengan kebutuhanmu!");
+        pilihRole = Console.ReadLine();
+        if (pilihRole == "1" || pilihRole == "2")
+        {
+        Menu.Menus.menuLogin();
+        }
+
+        Console.WriteLine("Pilih:");
+        pilihMenu = Console.ReadLine();
+        if(pilihMenu == "1" && pilihRole == "1")
+        {
+            Console.WriteLine("Login Sukses!");
+            Menu.Menus.menuPasien();
+        }else if(pilihMenu == "2" && pilihRole == "1")
+        {
+            Console.WriteLine("Daftar Sukses!");
+            Menu.Menus.menuPasien();
+        }else if(pilihMenu == "1" && pilihRole == "2")
+        {
+            Console.WriteLine("Login Sukses!");
+            Menu.Menus.menuNakes();
+        }else if (pilihMenu == "2" && pilihRole == "2")
+        {
+            Console.WriteLine("Daftar Sukses!");
+            Menu.Menus.menuNakes();
+        }
+
+        
+    }
+}
